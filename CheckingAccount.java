9bb1efc72fe2dde76dcd02aba4cafcde56e65a50
Avlie <antipovicana@gmail.com>
2025-11-01 17:57:26 +0200

    Dodano klasę CheckingAccount z metodą transfer, umożliwiającą przelewy między kontami.

---
+++ CheckingAccount.java
@@ -0,0 +1,17 @@
+public class CheckingAccount extends Account implements Transferable {
+
+    public CheckingAccount(String accountNumber, double initialBalance, Client owner) {
+        super(accountNumber, initialBalance, owner);
+    }
+
+    @Override
+    public void transfer(Account destinationAccount, double amount) {
+        if (amount > 0 && amount <= this.balance) {
+            this.balance -= amount;
+            destinationAccount.deposit(amount);
+            System.out.println("Przelew " + amount + " z konta biezacego " + this.accountNumber + " na konto " + destinationAccount.getAccountNumber() + " zostal zrealizowany.");
+        } else {
+            System.out.println("Przelew nieudany. Niewystarczajace srodki lub nieprawidlowa kwota.");
+        }
+    }
+}
